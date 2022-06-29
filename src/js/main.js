675df10e579ad68a573f25b8d25e5228506d4c1a
Aleksandra <aleksandra.rewer@gmail.com>
2022-06-29 18:42:44 +0200

    Created initial function in main.js for mobile hamburger-menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,26 @@
-const test = () => {
-	console.log('ok')
+const burgerBtn = document.querySelector('.burger-box__bars')
+const mobileMenu = document.querySelector('.mobile-menu')
+const hamburgerIcon = document.querySelector('.burger-box__bars-btn-hamburger')
+const exitIcon = document.querySelector('.burger-box__bars-btn-exit')
+
+
+const handleMenu = () => {
+	mobileMenu.style.display="block";
+	if(hamburgerIcon.classList.contains('active')){
+		hamburgerIcon.classList.toggle('active')
+		hamburgerIcon.classList.toggle('hidden')
+		exitIcon.classList.toggle('hidden')
+		exitIcon.classList.toggle('active')
+	} else if(exitIcon.classList.contains('active')){
+		exitIcon.classList.toggle('active')
+		exitIcon.classList.toggle('hidden')
+		hamburgerIcon.classList.toggle('active')
+		hamburgerIcon.classList.toggle('hidden')
+		mobileMenu.style.display="none";
+	}
+
+
+	
 }
+
+burgerBtn.addEventListener('click', handleMenu)
