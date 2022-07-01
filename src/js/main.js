fee03db476917580c475dcac8f63edb2e16b2464
Aleksandra <aleksandra.rewer@gmail.com>
2022-07-01 15:49:13 +0200

    Fixed functions for mobile sliding menu, added animation for its links and changed the resolution of the image in 'about-us' section

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,53 @@
 const burgerBtn = document.querySelector('.burger-box__bars')
-const mobileMenu = document.querySelector('.mobile-menu')
+const mobileMenu = document.querySelector('.header__menu')
 const hamburgerIcon = document.querySelector('.burger-box__bars-btn-hamburger')
 const exitIcon = document.querySelector('.burger-box__bars-btn-exit')
-
+const allMenuLinks = document.querySelectorAll('.header__menu-link')
 
 const handleMenu = () => {
-	mobileMenu.style.display="block";
-	if(hamburgerIcon.classList.contains('active')){
+	mobileMenu.classList.toggle('header__menu--active')
+
+	if (hamburgerIcon.classList.contains('active')) {
 		hamburgerIcon.classList.toggle('active')
 		hamburgerIcon.classList.toggle('hidden')
 		exitIcon.classList.toggle('hidden')
 		exitIcon.classList.toggle('active')
-	} else if(exitIcon.classList.contains('active')){
+	} else if (exitIcon.classList.contains('active')) {
 		exitIcon.classList.toggle('active')
 		exitIcon.classList.toggle('hidden')
 		hamburgerIcon.classList.toggle('active')
 		hamburgerIcon.classList.toggle('hidden')
-		mobileMenu.style.display="none";
 	}
 
+	allMenuLinks.forEach(link => {
+		link.addEventListener('click', () => {
+			mobileMenu.classList.remove('header__menu--active')
+			exitIcon.classList.add('hidden')
+			exitIcon.classList.remove('active')
 
-	
+			hamburgerIcon.classList.remove('hidden')
+			hamburgerIcon.classList.add('active')
+		})
+	})
+
+	handleLinksAnimation()
+}
+
+const handleLinksAnimation = () => {
+	let delayTime = 0
+
+	allMenuLinks.forEach(link => {
+		link.classList.toggle('links-animation')
+		link.style.animationDelay = `.${delayTime}s`
+		delayTime++
+		link.addEventListener('click', deleteLinksAnimation)
+	})
+}
+
+const deleteLinksAnimation = () => {
+	allMenuLinks.forEach(link => {
+		link.classList.remove('links-animation')
+	})
 }
 
 burgerBtn.addEventListener('click', handleMenu)
