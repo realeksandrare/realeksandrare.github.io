4086cf08f9cc8061cc13c3bdf2e35a5a9bb4c739
Aleksandra <aleksandra.rewer@gmail.com>
2022-07-06 14:50:17 +0200

    Fixed burger menu - page underneath does not move anymore

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,8 @@ const mobileMenu = document.querySelector('.header__menu')
 const hamburgerIcon = document.querySelector('.burger-box__bars-btn-hamburger')
 const exitIcon = document.querySelector('.burger-box__bars-btn-exit')
 const allMenuLinks = document.querySelectorAll('.header__menu-link')
+const currentYear = document.querySelector('.current-year')
+
 
 const handleMenu = () => {
 	mobileMenu.classList.toggle('header__menu--active')
@@ -12,11 +14,13 @@ const handleMenu = () => {
 		hamburgerIcon.classList.toggle('hidden')
 		exitIcon.classList.toggle('hidden')
 		exitIcon.classList.toggle('active')
+		document.body.classList.toggle('body-hidden')
 	} else if (exitIcon.classList.contains('active')) {
 		exitIcon.classList.toggle('active')
 		exitIcon.classList.toggle('hidden')
 		hamburgerIcon.classList.toggle('active')
 		hamburgerIcon.classList.toggle('hidden')
+		document.body.classList.toggle('body-hidden')
 	}
 
 	allMenuLinks.forEach(link => {
@@ -50,29 +54,29 @@ const deleteLinksAnimation = () => {
 	})
 }
 
-const API_KEY = 'AIzaSyAF9Q8amBVF05M0WSlbfaazmOZBI8K8b24';
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear()
+	currentYear.innerText = year
+}
+
+const API_KEY = 'AIzaSyAF9Q8amBVF05M0WSlbfaazmOZBI8K8b24'
 const URL = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&callback=initMap`
 
 function initMap() {
-
-  const park = { lat: 53.473658, lng: 22.657368 };
-  // The map, centered at Uluru
-  const map = new google.maps.Map(document.querySelector('#map'), {
-    zoom: 10,
-    center: park,
-  });
-  // The marker, positioned at Uluru
-  const marker = new google.maps.Marker({
-    position: park,
-    map: map,
-  });
+	const park = { lat: 53.473658, lng: 22.657368 }
+	// The map, centered at Uluru
+	const map = new google.maps.Map(document.querySelector('#map'), {
+		zoom: 10,
+		center: park,
+	})
+	// The marker, positioned at Uluru
+	const marker = new google.maps.Marker({
+		position: park,
+		map: map,
+	})
 }
 
-window.initMap = initMap;
+window.initMap = initMap
 
-
-
-
-
-
+handleCurrentYear()
 burgerBtn.addEventListener('click', handleMenu)
